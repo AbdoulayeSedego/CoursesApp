163dc01a65fd9dd2d8f1f6d0ad6b1c836883a0ce
Abdoulaye Sedego <abdoulaye.sedego95@gmail.com>
2023-03-07 23:57:33 -0500

    bug fix

--- app/src/main/java/com/example/coursesapp/data/Datasource.kt
+++ app/src/main/java/com/example/coursesapp/data/Datasource.kt
@@ -6,20 +6,20 @@ import com.example.coursesapp.model.Courses
 class Datasource() {
     fun loadImages() : List<Courses> {
         return listOf<Courses>(
-            Courses(R.string.architecture, R.drawable.architecture),
-            Courses(R.string.business, R.drawable.business),
-            Courses(R.string.crafts, R.drawable.crafts),
-            Courses(R.string.design, R.drawable.design),
-            Courses(R.string.culinary, R.drawable.culinary),
-            Courses(R.string.drawing, R.drawable.drawing),
-            Courses(R.string.fashion, R.drawable.fashion),
-            Courses(R.string.film, R.drawable.film),
-            Courses(R.string.gaming, R.drawable.gaming),
-            Courses(R.string.tech, R.drawable.tech),
-            Courses(R.string.photography, R.drawable.photography),
-            Courses(R.string.painting, R.drawable.painting),
-            Courses(R.string.music, R.drawable.music),
-            Courses(R.string.lifestyle, R.drawable.lifestyle),
+            Courses(R.string.architecture, 58, R.drawable.architecture),
+            Courses(R.string.crafts, 121, R.drawable.crafts),
+            Courses(R.string.business, 78, R.drawable.business),
+            Courses(R.string.culinary, 118, R.drawable.culinary),
+            Courses(R.string.design, 423, R.drawable.design),
+            Courses(R.string.fashion, 92, R.drawable.fashion),
+            Courses(R.string.film, 165, R.drawable.film),
+            Courses(R.string.gaming, 164, R.drawable.gaming),
+            Courses(R.string.drawing, 326, R.drawable.drawing),
+            Courses(R.string.lifestyle, 305, R.drawable.lifestyle),
+            Courses(R.string.music, 212, R.drawable.music),
+            Courses(R.string.painting, 172, R.drawable.painting),
+            Courses(R.string.photography, 321, R.drawable.photography),
+            Courses(R.string.tech, 118, R.drawable.tech)
 
         )
     }
